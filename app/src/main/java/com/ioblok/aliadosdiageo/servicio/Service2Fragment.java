8e4372c477647a8231f9f77fed3c7a1b05a7d83b
kreativeco <tlaxqueno.guerrero.j.c@gmail.com>
2016-03-12 17:42:56 -0600

    add las menu (services) with tabs, add suport library design on gradle, add views and fragments for services menu

---
+++ app/src/main/java/com/ioblok/aliadosdiageo/servicio/Service2Fragment.java
@@ -0,0 +1,24 @@
+package com.ioblok.aliadosdiageo.servicio;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ioblok.aliadosdiageo.R;
+
+/**
+ * Created by kreativeco on 12/03/16.
+ */
+public class Service2Fragment extends Fragment {
+    View v;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.service2_fragment, null);
+
+        return v;
+
+    }
+}
