4d562d61a253b6031c3d0c7445f92b722cb7fff7
Ruben Hernandez <deconstruim@gmail.com>
2016-03-18 04:40:02 -0600

    Botones de seccion whisky primer parte

---
+++ app/src/main/java/com/ioblok/aliadosdiageo/categorias/content_whisky/BuchanansMaster/BuchanansMasterMensajesFragment.java
@@ -0,0 +1,56 @@
+package com.ioblok.aliadosdiageo.categorias.content_whisky.BuchanansMaster;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bluejamesbond.text.DocumentView;
+import com.bluejamesbond.text.style.TextAlignment;
+import com.ioblok.aliadosdiageo.R;
+
+public class BuchanansMasterMensajesFragment extends Fragment {
+    String text,textdos,texttres;
+
+    View v;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.fragment_buchanans_master_mensajes, null);
+        initialize();
+        return v;
+    }
+
+    public void initialize(){
+        Typeface myFont = Typeface.createFromAsset(getActivity().getAssets(), "acaslonpro-regular-webfont.ttf");
+        text = getString(R.string.title_buchanans_master_one);
+        textdos = getString(R.string.title_buchanans_master_dos);
+        texttres = getString(R.string.title_buchanans_master_tres);
+
+        DocumentView documentView = (DocumentView)v.findViewById(R.id.txt_bl_one);
+        DocumentView documentViewtwo = (DocumentView)v.findViewById(R.id.txt_bl_two);
+        DocumentView documentViewthree = (DocumentView)v.findViewById(R.id.txt_bl_three);
+
+        documentView.setText(text);
+        documentView.getDocumentLayoutParams().setTextAlignment(TextAlignment.JUSTIFIED);
+        documentView.getDocumentLayoutParams().setTextColor(0xFF333333);
+
+        documentView.getDocumentLayoutParams().setTextTypeface(myFont);
+
+        documentViewtwo.setText(textdos);
+        documentViewtwo.getDocumentLayoutParams().setTextAlignment(TextAlignment.JUSTIFIED);
+        documentViewtwo.getDocumentLayoutParams().setTextColor(0xFF333333);
+
+        documentViewtwo.getDocumentLayoutParams().setTextTypeface(myFont);
+
+        documentViewthree.setText(texttres);
+        documentViewthree.getDocumentLayoutParams().setTextAlignment(TextAlignment.JUSTIFIED);
+        documentViewthree.getDocumentLayoutParams().setTextColor(0xFF333333);
+
+        documentViewthree.getDocumentLayoutParams().setTextTypeface(myFont);
+    }
+
+}
